ee6269d6df057acaf4c3cf981e38793b5c031127
hwayuya <hwayuya@gmail.com>
2020-06-15 01:46:04 +0900

    플레이어 라이프 오류 수정 게임 BGM추가

--- UnityProject/Assets/나희정/Scripts/PlayBgm.cs
+++ UnityProject/Assets/나희정/Scripts/PlayBgm.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayBgm : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //BGMMgr.Instance
-    }
+
+   public  GameObject boss;
+    public GameObject bgm1;
+    public GameObject bgm2;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (boss.activeSelf == false)
+        {
+            if (bgm2.activeSelf == true)
+            {
+                bgm2.SetActive(false);
+            }
+
+            bgm1.SetActive(true);
+        }
+
+        else
+        {
+            if (bgm1.activeSelf == true)
+            {
+                bgm1.SetActive(false);
+            }
+
+            bgm2.SetActive(true);
+        }
     }
 }
